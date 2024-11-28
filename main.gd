9aa21d043d0727d5ec23fad79020b559e62820dd
DefineSleep <123536705+DefineSleep@users.noreply.github.com>
2024-11-28 04:37:25 -0500

    added so much shit

--- main.gd
+++ main.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var texture_rect: TextureRect = $TextureRect # background 
+
 
 @onready var time_label: Label = $PanelContainer/time_label
 
@@ -18,7 +18,9 @@ extends Control
 @onready var processor_name: Label = $HBoxContainer/processor_name
 @onready var processor_cores: Label = $HBoxContainer/processor_cores
 
+@onready var current_task: Label = $curent_task_container/current_task
 
+@onready var date_label: Label = $date_label
 
 
 # save system 
@@ -39,19 +41,58 @@ var hours : int
 var standing : bool = false
 # -----
 
-
+# --------------------------------
 
 func _ready() -> void:
+	OS.alert("USE WINDOWS KEY + LEFT or RIGHT TO MOVE WINDOW","README")
 	pass 
 
 
 
 func _process(delta: float) -> void:
-	update_memory_labels()
 	time_label.text = Time.get_time_string_from_system()
 	current_task_logic(Time.get_time_string_from_system())
 	standing_counter_text.text = str(miliseconds)
 	standing_counter_code()
+	print_schedule(Time.get_datetime_dict_from_system(false).hour)
+	test_function()
+
+	current_task.text = print_schedule(Time.get_datetime_dict_from_system(false).hour)
+	update_time_date()
+	
+# --------------------------------
+
+func test_function():
+	# have all print functions here for testing
+	#OS.get_locale() -> "en_us"
+	#OS.get_granted_permissions() -> []
+	#OS.get_distribution_name() -> "Windows"
+	
+	#printerr()
+	pass
+
+# --------------------------------
+
+
+func update_time_date():
+	var current_time = Time.get_datetime_dict_from_system()
+	date_label.text = weekday_setter(current_time.weekday)
+	print(weekday_setter(current_time.weekday))
+	
+	pass
+	
+	
+
+func weekday_setter(_weekday):
+	match _weekday:
+		1: return "Monday"
+		2: return "Tuesday"
+		3: return "Wednesday"
+		4: return "Thursday"
+		5: return "Friday"
+		6: return "Saturday"
+		7: return "Sunday"
+		_: return "error"
 
 func update_memory_labels(): # percentage_full = (curreent_amount/max_amount) * 100 
 	processor_name.text = OS.get_processor_name()
@@ -67,7 +108,6 @@ func update_memory_labels(): # percentage_full = (curreent_amount/max_amount) * 
 
 
 func standing_counter_code():
-
 	standing_counter_text.text = str(hours).pad_zeros(2) + ": " + str(minutes).pad_zeros(2) + ": " + str(seconds).pad_zeros(2) + ": " + str(miliseconds).pad_zeros(2)
 	if standing:
 		miliseconds += 1
@@ -96,11 +136,12 @@ func current_task_logic(time):
 
 
 func _on_shortcut_button_1_pressed() -> void:
-	OS.shell_open("https://godotengine.org")
+	OS.shell_open("https://docs.godotengine.org/en/stable")
 
 
 func _on_shortcut_button_2_pressed() -> void:
 	OS.shell_open("https://youtube.com")
+	#OS.shell_open("https://gmail.com")
 
 
 func _on_exit_button_pressed() -> void:
@@ -113,7 +154,80 @@ func _on_reset_button_pressed() -> void:
 	minutes = 0
 	hours = 0
 	
+
+func _on_update_timer_timeout() -> void:
+	update_memory_labels()
+	pass
 	
 # ------
 # loading and saving 
 # ------
+
+
+func data_save():
+	pass
+	
+	
+	
+func data_load():
+	pass
+	
+	
+	
+	
+# ------
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	if toggled_on == true : 
+		OS.low_processor_usage_mode = true
+	if toggled_on == false : 
+		OS.low_processor_usage_mode = false
+
+
+# ------
+# SCHEDULE N SHIT
+# ------
+# code a way to enter text and setup slots 
+var slot_1 : String = "slot 1"
+var slot_2 : String = "slot 2"
+var slot_3 : String = "slot 3"
+var slot_4 : String = "slot 4"
+
+
+func print_schedule(_time): # tie to a timer of 60 seconds 
+	var current_task : String 
+	#scehdule 
+	match _time:
+		18:current_task = "Morning routine"
+		19:current_task = "Freetime , Setup Slots"
+		20:current_task = "Freetime , Coffee"
+		21:current_task = slot_1
+		22:current_task = slot_1
+		23:current_task = "Freetime"
+		0:current_task = "Workout"
+		1:current_task = "Drawing"
+		2:current_task = slot_2
+		3:current_task = slot_2
+		4:current_task = "Freetime"
+		5:current_task = slot_3
+		6:current_task = "Food / Freetime"
+		7:current_task = slot_4 + "| Finish work"
+		8:current_task = slot_4
+		9:current_task = "Bedtime routine"
+		10:current_task = "Sleep"
+		11:current_task = "Sleep"
+		12:current_task = "Sleep"
+		13:current_task = "Sleep"
+		14:current_task = "Sleep"
+		15:current_task = "Sleep"
+		16:current_task = "Sleep"
+		17:current_task = "Sleep"
+		
+		
+		
+		
+		_:current_task = "ERROR : undefined"
+	
+	
+	return current_task
+	
